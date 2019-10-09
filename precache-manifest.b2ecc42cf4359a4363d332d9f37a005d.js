8617138a2849cee7675f8927e647b50fd997bfe0
Patrick Hanson <patrickhanson08@gmail.com>
2019-10-09 15:48:03 -0400

    Updates

---
+++ precache-manifest.b2ecc42cf4359a4363d332d9f37a005d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bb98c8007438f8acead8e5c4ec6b83",
+    "url": "/marvelsearch/index.html"
+  },
+  {
+    "revision": "94b487d91050df0b2e08",
+    "url": "/marvelsearch/static/css/main.5b52635d.chunk.css"
+  },
+  {
+    "revision": "65ba888891912db3d4b8",
+    "url": "/marvelsearch/static/js/2.2392efd2.chunk.js"
+  },
+  {
+    "revision": "94b487d91050df0b2e08",
+    "url": "/marvelsearch/static/js/main.a0a94c66.chunk.js"
+  },
+  {
+    "revision": "2ce19fe80cbd57f2fe8d",
+    "url": "/marvelsearch/static/js/runtime-main.47a92b2c.js"
+  },
+  {
+    "revision": "0392e6da2a986c54084d8e2b60a491e3",
+    "url": "/marvelsearch/static/media/Font-Marvel-Logo.0392e6da.jpg"
+  }
+]);
